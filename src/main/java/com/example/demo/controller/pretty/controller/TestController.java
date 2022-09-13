8782bcda5c8d4c33a0893407b11b3a626d1f8990
wuyingbin <yb.ng@foxmail.com>
2022-09-13 17:18:51 +0800

    使用日志框架

--- src/main/java/com/example/demo/controller/pretty/controller/TestController.java
+++ src/main/java/com/example/demo/controller/pretty/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller.pretty.controller;
 import com.example.demo.controller.pretty.dto.TestDTO;
 import com.example.demo.controller.pretty.dto.ValidateGetDTO;
 import com.example.demo.controller.pretty.service.TestService;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -17,6 +18,7 @@ import javax.validation.constraints.NotBlank;
  * @author WuYingBin
  * date: 2022/7/13
  */
+@Slf4j
 @RestController(value = "prettyTestController")
 @RequestMapping("/pretty")
 @Validated
@@ -31,18 +33,22 @@ public class TestController {
 
     @GetMapping("/{num}")
     public Integer detail(@PathVariable("num") @Min(1) @Max(20) Integer num) {
-        return num * num;
+        int result = num * num;
+        log.info("运算结果: {}", result);
+        return result;
     }
 
     @GetMapping("/getByEmail")
     public TestDTO getByAccount(@RequestParam @NotBlank @Email String email) {
         TestDTO testDTO = new TestDTO();
         testDTO.setEmail(email);
+        log.info("账号email: {}", email);
         return testDTO;
     }
 
     @GetMapping(value = "/returnString"/*, produces = "application/json; charset=UTF-8"*/)
     public String returnString() {
+        log.info("success");
         return "success";
     }
 
@@ -50,20 +56,20 @@ public class TestController {
     public String testValidateGetParam(@RequestParam @NotBlank String userName,
                                        @RequestParam @Length(min = 6, max = 20) String password,
                                        @RequestParam @Email String email) {
-        System.out.printf("userName: %s, password: %s, email: %s", userName, password, email);
+        log.info("userName: {}, password: {}, email: {}", userName, password, email);
         return "success";
     }
 
     //上面的请求参数太多，写得有点臃肿，可以优化成以下形式
     @GetMapping("/test-validate-get-dto")
     public String testValidateGetDTO(@Validated ValidateGetDTO validateGetDTO) {
-        System.out.println(validateGetDTO);
+        log.info("数据: {}", validateGetDTO);
         return "success";
     }
 
     @GetMapping("/test-get-request-body")
     public TestDTO testGetRequestBody(@RequestBody TestDTO testDTO) {
-        System.out.println("获取请求体数据: " + testDTO);
+        log.info("获取请求体数据: {}", testDTO);
         return testDTO;
     }
 
--- src/main/java/com/example/demo/controller/pretty/service/TestService.java
+++ src/main/java/com/example/demo/controller/pretty/service/TestService.java
@@ -1,16 +1,18 @@
 package com.example.demo.controller.pretty.service;
 
 import com.example.demo.controller.pretty.dto.TestDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * @author WuYingBin
  * date: 2022/7/13
  */
+@Slf4j
 @Service(value = "prettyTestService")
 public class TestService {
 
     public void save(TestDTO testDTO) {
-        System.out.println("保存成功");
+        log.info("保存成功: {}", testDTO);
     }
 }
